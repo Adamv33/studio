61b9fb6d044d8bfbd9a7c810bbe26727c1515f13
Adam Voydik <adam@emskillz.com>
2025-06-08 00:05:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/instructors/[id]/page.tsx
+++ src/app/instructors/[id]/page.tsx
@@ -14,6 +14,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { InstructorForm } from '@/components/instructors/InstructorForm';
 import { useToast } from "@/hooks/use-toast"
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { format } from 'date-fns';
 
